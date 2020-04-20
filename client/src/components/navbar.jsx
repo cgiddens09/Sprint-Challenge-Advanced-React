48b8eb865c8226761db0a706a599940bd7ba6313
Christina Giddens <giddens.christina@gmail.com>
2020-04-19 21:59:59 -0400

    Working Dark Mode, no testing

--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -3,22 +3,30 @@ import useDarkMode from '../hooks/useDarkMode';
 
 
 const Navbar = () => {
-  const [darkMode, setDarkMode] = useDarkMode(false);
+  const [darkMode, setDarkMode] = useDarkMode();
   const toggler = e =>{
       e.preventDefault();
       setDarkMode(!darkMode);
   };
 
-  return (
-    <nav>
+  
+    return (
+        <nav className="navbar">
         <h1>Players</h1>
-        <button onclick={toggler} >Dark Mode</button>
-    </nav>
+        <div className="dark-mode__toggle">
+            <div
+            onClick={toggler}
+            className={darkMode ? 'toggle toggled' : 'toggle'}
+            />
+        </div>
+        </nav>
+  );
+    
 
     
 
 
-  );
+  
 };
 
 export default Navbar;
